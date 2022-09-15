00f8e77e74f4b1bfa9a9ef9f65cb3d803a9e5ae7
Brian Swartz <bdswartz72@gmail.com>
2022-09-15 09:16:57 -0400

    bare server and data file complete

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const transactionRoutes = require('./transactionRoutes');
+
+router.use(transactionRoutes);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+// API path
+const apiRoutes = require('./controllers/api');
+
+const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+// initiate routing
+app.use('/api', apiRoutes);
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
